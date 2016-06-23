b7250ff72be1c123a166c3ca8fcfbe4672257ccd
Małgorzata Dziubich <malgorzata.dziubich@mbank.pl>
2016-06-23 20:05:09 +0200

    Added initial files

---
+++ Warsaw_Tram/MapViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MapViewController.swift
+//  Warsaw_Tram
+//
+//  Created by Małgorzata Dziubich on 23/06/16.
+//  Copyright © 2016 Małgorzata Dziubich. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        super.viewDidLoad()
+        addHomeAnnotation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0] as CLLocation
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta: CLLocationDegrees = 0.01
+        let lonDelta: CLLocationDegrees = 0.01
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        self.map.setRegion(region, animated: true)
+    }
+    
+    private func addHomeAnnotation() {
+        let latitude: CLLocationDegrees = 52.453946
+        let longitude: CLLocationDegrees = 19.005797
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Home"
+        annotation.subtitle = "Nowiny"
+        map.addAnnotation(annotation)
+    }
+}
---
+++ Warsaw_Tram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warsaw_Tram
+//
+//  Created by Małgorzata Dziubich on 23/06/16.
+//  Copyright © 2016 Małgorzata Dziubich. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
