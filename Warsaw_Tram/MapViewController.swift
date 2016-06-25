4d2cebfb227d06358140c0937873905a8e611f1b
Małgorzata Dziubich <malgorzata.dziubich@mbank.pl>
2016-06-25 08:51:06 +0200

    show trams nd user location on map

--- Warsaw_Tram/MapViewController.swift
+++ Warsaw_Tram/MapViewController.swift
@@ -10,50 +10,147 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class MapViewController: UIViewController {
 
     @IBOutlet var map: MKMapView!
     
     var locationManager = CLLocationManager()
+    var alertHelper: AlertHelper?
+    var userLocationDefined: Bool = false
+    var userLocation: CLLocationCoordinate2D?
+    
+    let resourceId = "id=c7238cfe-8b1f-4c38-bb4a-de386db7e776"
+    let warsawTramsApiKey = "apikey=060b903c-b0c3-427e-934d-9e4a81a61969"
+    let mapLatDelta: CLLocationDegrees = 0.05
+    let mapLonDelta: CLLocationDegrees = 0.05
     
     override func viewDidLoad() {
-        
+        alertHelper = AlertHelper()
+        setupMap()
+        setupLocationManager()
+        super.viewDidLoad()
+    }
+    
+    @IBAction func showCurrentLocation(sender: AnyObject) {
+        showUserCurrentLocation()
+    }
+    
+    private func setupLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+    }
+    
+    private func setupMap() {
+        navigationController?.navigationBar.backgroundColor = UIColor.orangeColor()
+        map.showsScale = true
+    }
+    
+    private func fetchTramsData() {
+        let stringURL : NSString = "https://api.um.warszawa.pl/api/action/wsstore_get?\(resourceId)&\(warsawTramsApiKey)"
+        let url : NSURL = NSURL(string: stringURL as String)!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "GET"
         
-        super.viewDidLoad()
-        addHomeAnnotation()
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+            if error != nil {
+                self.goToMainDesktop(errorMessage: error!.localizedDescription)
+            } else if data != nil {
+                guard let jsonResult = (try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as? NSDictionary,
+                    let result: NSArray = jsonResult["result"] as? NSArray else {
+                        return self.goToMainDesktop(errorMessage: "Brak wyników")
+                }
+                
+                for tram in result {
+                    if tram["Status"] as? String == "RUNNING" {
+                        if let line = tram["FirstLine"] as? String,
+                            let lat = tram["Lat"] as? CLLocationDegrees,
+                            let lon = tram["Lon"] as? CLLocationDegrees,
+                            let lowFloor = tram["LowFloor"] as? Bool {
+                                dispatch_async(dispatch_get_main_queue(), { 
+                                    self.addTramAnnotation(line, lat: lat, lon: lon, lowFloor: lowFloor)
+                                })
+                        }
+                    }
+                }
+            } else {
+                self.goToMainDesktop(errorMessage: "Brak aktywnych tramwaji")
+            }
+        }
+        task.resume()
+    }
+    
+    private func addTramAnnotation(lineNumber: String, lat: CLLocationDegrees, lon: CLLocationDegrees, lowFloor: Bool) {
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, lon)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = lineNumber
+        annotation.subtitle = lowFloor ? "Tramwaj niskopodłogowy" : nil
+        map.addAnnotation(annotation)
+    }
+    
+    // Zoom map to current location
+    private func showUserCurrentLocation() {
+        if let location = self.userLocation {
+            let span = MKCoordinateSpanMake(mapLatDelta, mapLonDelta)
+            let region = MKCoordinateRegion(center: location, span: span)
+            self.map.setRegion(region, animated: true)
+        }
+    }
+    
+    private func goToMainDesktop(errorMessage error: String) {
+        print("error: \(error)")
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) {
+            UIAlertAction in
+            self.performSegueWithIdentifier("showMainDesktop", sender: nil)
+        }
+        
+        alertHelper?.showAlert("Błąd", message: error, okAction: okAction, presentingViewController: self)
+    }
+}
+
+// MARK: CLLocationManagerDelegate
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        //  Check access for user location
+        if status == .AuthorizedWhenInUse {
+            locationManager.requestLocation()
+            fetchTramsData()
+        } else {
+            showSettingsAlert()
+        }
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation: CLLocation = locations[0] as CLLocation
-        let latitude = userLocation.coordinate.latitude
-        let longitude = userLocation.coordinate.longitude
-        let latDelta: CLLocationDegrees = 0.01
-        let lonDelta: CLLocationDegrees = 0.01
-        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
-        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-        let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
-        
-        self.map.setRegion(region, animated: true)
+        if let location = locations.first {
+            self.userLocation = location.coordinate
+            if !userLocationDefined {
+                showUserCurrentLocation()
+                userLocationDefined = true
+            }
+        }
     }
     
-    private func addHomeAnnotation() {
-        let latitude: CLLocationDegrees = 52.453946
-        let longitude: CLLocationDegrees = 19.005797
-        let latDelta: CLLocationDegrees = 0.05
-        let lonDelta: CLLocationDegrees = 0.05
-        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
-        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
-        map.setRegion(region, animated: true)
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: \(error)")
+    }
+    
+    private func showSettingsAlert() {
+        // Create the actions buttons for settings alert
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) {
+            UIAlertAction in
+            UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
+            NSLog("OK Pressed")
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) {
+            UIAlertAction in
+            NSLog("Cancel Pressed")
+            self.performSegueWithIdentifier("showMainDesktop", sender: nil)
+        }
         
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Home"
-        annotation.subtitle = "Nowiny"
-        map.addAnnotation(annotation)
+        alertHelper?.showAlert("Błąd", message: "Brak dostępu do usługi lokalizacji. Czy chcesz zmienić ustawienia teraz?", okAction: okAction, cancelAction: cancelAction, presentingViewController: self)
     }
 }
