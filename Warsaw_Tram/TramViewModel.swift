1a30aaa55614c91c324ac8c7c278d9c72fed927b
Małgorzata Dziubich <malgorzata.dziubich@mbank.pl>
2016-07-14 14:22:01 +0200

    Additional tests for VM

--- Warsaw_Tram/TramViewModel.swift
+++ Warsaw_Tram/TramViewModel.swift
@@ -8,50 +8,11 @@
 
 import UIKit
 
-protocol TramViewModelProtocol {
-    
-    func getTramsData(success: [Tram] -> Void, failure: String -> Void)
-}
-
-class TramViewModel: TramViewModelProtocol {
+class TramViewModel {
     
     let resourceId = "id=c7238cfe-8b1f-4c38-bb4a-de386db7e776"
     let warsawTramsApiKey = "apikey=060b903c-b0c3-427e-934d-9e4a81a61969"
     var session = NSURLSession.self
-
-    func getTramsData(success: [Tram] -> Void, failure: String -> Void) {
-
-        var trams = [Tram]()
-        let stringURL = "https://api.um.warszawa.pl/api/action/wsstore_get?\(resourceId)&\(warsawTramsApiKey)"
-        let url = NSURL(string: stringURL)!
-        
-        let request = NSMutableURLRequest(URL: url)
-        request.HTTPMethod = "GET"
-        
-        let task = session.sharedSession().dataTaskWithRequest(request) { data, response, error in
-            
-            guard let taskError = error?.localizedDescription else {
-                do {
-                    guard let taskData = data,
-                        let jsonResult = try NSJSONSerialization.JSONObjectWithData(taskData, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary,
-                        let result: Array = jsonResult["result"] as? Array<AnyObject> else {
-                            return failure("No results")
-                        }
-
-                    for tram in result {
-                        if let singleTram = tram as? [String : AnyObject] {
-                            trams.append(Tram(tram: singleTram)!)
-                        }
-                    }
-                } catch let error as NSError {
-                    return failure(error.localizedDescription)
-                }
-                return success(trams)
-            }
-            failure(taskError)
-        }
-        task.resume()
-    }
     
     func fetchTramsNumbers(success: (trams: [Int], lowFloorTrams: [Int]) -> Void, failure: String -> Void) {
         getTramsData({ (trams) in
@@ -95,5 +56,39 @@ class TramViewModel: TramViewModelProtocol {
         }, failure: { (error) in
             failure(error)
         })
+    }
+    
+    private func getTramsData(success: [Tram] -> Void, failure: String -> Void) {
+        
+        var trams = [Tram]()
+        let stringURL = "https://api.um.warszawa.pl/api/action/wsstore_get?\(resourceId)&\(warsawTramsApiKey)"
+        let url = NSURL(string: stringURL)!
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "GET"
+        
+        let task = session.sharedSession().dataTaskWithRequest(request) { data, response, error in
+            
+            guard let taskError = error?.localizedDescription else {
+                do {
+                    guard let taskData = data,
+                        let jsonResult = try NSJSONSerialization.JSONObjectWithData(taskData, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary,
+                        let result: Array = jsonResult["result"] as? Array<AnyObject> else {
+                            return failure("No results")
+                    }
+                    
+                    for tram in result {
+                        if let singleTram = tram as? [String : AnyObject] {
+                            trams.append(Tram(tram: singleTram)!)
+                        }
+                    }
+                } catch let error as NSError {
+                    return failure(error.localizedDescription)
+                }
+                return success(trams)
+            }
+            failure(taskError)
+        }
+        task.resume()
     }
 }
--- Warsaw_TramTests/TramViewModelTest.swift
+++ Warsaw_TramTests/TramViewModelTest.swift
@@ -14,36 +14,125 @@ import CoreLocation
 class TramViewModelTest: XCTestCase {
 
     var tramViewModel: TramViewModel!
+    let data = ["result": [["Brigade": "6", "FirstLine": "1", "Lat": 52.2164497, "Lines": "1", "Lon": 21.005352, "LowFloor": 0, "Status": "RUNNING", "Time": "2016-07-01T21:27:43"], ["Brigade": "5", "FirstLine": "35 ", "Lat": 52.1678963, "Lines": "35             ", "Lon": 21.0159893, "LowFloor": 1, "Status": "RUNNING", "Time": "2016-07-01T21:27:43"], ["Brigade": "2   ", "FirstLine": "2  ", "Lat": 52.3192101, "Lines": "2              ", "Lon": 20.9513645, "LowFloor": 1, "Status": "FINISHED", "Time": "2016-07-01T21:27:43"]]]
     
     override func setUp() {
         tramViewModel = TramViewModel()
         super.setUp()
     }
     
-    func testGetTramsData() {
-        
-        let data = ["result": [["Brigade": "6", "FirstLine": "1", "Lat": 52.2164497, "Lines": "1", "Lon": 21.005352, "LowFloor": 0, "Status": "RUNNING", "Time": "2016-07-01T21:27:43"]]]
+    func testFetchTramsNumbers() {
         do {
             let jsonData = try NSJSONSerialization.dataWithJSONObject(data, options: [])
             let urlResponse = NSHTTPURLResponse(URL: NSURL(string: "https://api.um.warszawa.pl/api/action/wsstore_get")!, statusCode: 200, HTTPVersion: nil, headerFields: nil)
             StubNSURLSession.stubResponse = (jsonData, urlResponse: urlResponse, error: nil)
             tramViewModel.session = StubNSURLSession.self
             
-            let expectation = self.expectationWithDescription("testGetTramsData")
+            let expectation = self.expectationWithDescription("testFetchTramsNumbers")
+            
+            tramViewModel.fetchTramsNumbers({ (trams, lowFloorTrams) in
+                XCTAssertEqual(String(trams[0]), "1", "Incorrect tram number.")
+                XCTAssertEqual(String(trams[1]), "35", "Incorrect tram number.")
+                XCTAssertEqual(String(lowFloorTrams[0]), "35", "Incorrect low floor tram number.")
 
-            tramViewModel.getTramsData({ (trams) in
+                expectation.fulfill()
+
+            }, failure: { (error) in
+                XCTFail("unexpected error")
+                expectation.fulfill()
+            })
+            waitForExpectationsWithTimeout(0.5, handler: nil)
+        } catch {
+            print("tests error: \(error)")
+        }
+    }
+    
+    // Test should return selected tram nuber without low floor filter
+    func testFetchTramsForMap() {
+
+        let expectation = self.expectationWithDescription("testFetchTramsForMap")
+
+        do {
+            let jsonData = try NSJSONSerialization.dataWithJSONObject(data, options: [])
+            let urlResponse = NSHTTPURLResponse(URL: NSURL(string: "https://api.um.warszawa.pl/api/action/wsstore_get")!, statusCode: 200, HTTPVersion: nil, headerFields: nil)
+            StubNSURLSession.stubResponse = (jsonData, urlResponse: urlResponse, error: nil)
+            tramViewModel.session = StubNSURLSession.self
+            
+            let tramsParameters = DisplayedTramsData(tramNumber: "1", showAllTrams: false, lowFloorFilter: false)
+            tramViewModel.fetchTramsForMap(tramsParameters, success: { (trams) in
+                
                 XCTAssertEqual(trams[0].number, "1", "Wrong tram number.")
                 XCTAssertFalse(trams[0].lowFloor, "The tram lowFloor status should be false")
-                XCTAssertEqual(trams[0].status, "RUNNING", "Incorrect tram status.")
-                XCTAssertEqual(String(trams[0].latitude), "52.2164497", "Incorrect tram lattitude.")
-                XCTAssertEqual(String(trams[0].longitude), "21.005352", "Incorrect tram longitude.")
                 
                 expectation.fulfill()
 
+            }, failure: { (error) in
+                XCTFail("unexpected error")
+                expectation.fulfill()
+            })
+            
+            waitForExpectationsWithTimeout(0.5, handler: nil)
+        } catch {
+            print("tests error: \(error)")
+        }
+    }
+    
+    // Test should return all trams without low floor filter
+    func testFetchTramsForMap2() {
+        
+        let expectation = self.expectationWithDescription("testFetchTramsForMap2")
+        
+        do {
+            let jsonData = try NSJSONSerialization.dataWithJSONObject(data, options: [])
+            let urlResponse = NSHTTPURLResponse(URL: NSURL(string: "https://api.um.warszawa.pl/api/action/wsstore_get")!, statusCode: 200, HTTPVersion: nil, headerFields: nil)
+            StubNSURLSession.stubResponse = (jsonData, urlResponse: urlResponse, error: nil)
+            tramViewModel.session = StubNSURLSession.self
+            
+            let tramsParameters = DisplayedTramsData(tramNumber: "1", showAllTrams: true, lowFloorFilter: false)
+            tramViewModel.fetchTramsForMap(tramsParameters, success: { (trams) in
+                
+                XCTAssertEqual(trams[0].number, "1", "Wrong tram number.")
+                XCTAssertEqual(trams[1].number, "35", "Wrong tram number.")
+                XCTAssertFalse(trams[0].lowFloor, "The tram lowFloor status should be false")
+                XCTAssertTrue(trams[1].lowFloor, "The tram lowFloor status should be true")
+                
+                expectation.fulfill()
+                
                 }, failure: { (error) in
                     XCTFail("unexpected error")
                     expectation.fulfill()
             })
+            
+            waitForExpectationsWithTimeout(0.5, handler: nil)
+        } catch {
+            print("tests error: \(error)")
+        }
+    }
+    
+    // Test should return selected tram nuber with low floor filter
+    func testFetchTramsForMap3() {
+        
+        let expectation = self.expectationWithDescription("testFetchTramsForMap3")
+        
+        do {
+            let jsonData = try NSJSONSerialization.dataWithJSONObject(data, options: [])
+            let urlResponse = NSHTTPURLResponse(URL: NSURL(string: "https://api.um.warszawa.pl/api/action/wsstore_get")!, statusCode: 200, HTTPVersion: nil, headerFields: nil)
+            StubNSURLSession.stubResponse = (jsonData, urlResponse: urlResponse, error: nil)
+            tramViewModel.session = StubNSURLSession.self
+            
+            let tramsParameters = DisplayedTramsData(tramNumber: "35", showAllTrams: false, lowFloorFilter: true)
+            tramViewModel.fetchTramsForMap(tramsParameters, success: { (trams) in
+                
+                XCTAssertEqual(trams[0].number, "35", "Wrong tram number.")
+                XCTAssertTrue(trams[0].lowFloor, "The tram lowFloor status should be true")
+                
+                expectation.fulfill()
+                
+                }, failure: { (error) in
+                    XCTFail("unexpected error")
+                    expectation.fulfill()
+            })
+            
             waitForExpectationsWithTimeout(0.5, handler: nil)
         } catch {
             print("tests error: \(error)")
